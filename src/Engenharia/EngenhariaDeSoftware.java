ca61f1cc203004aa912b41968acd88cbdc51f9c8
paulo.artur <paulo.artur@LAB5-DCX-17.dcx.ufpb.br>
2016-10-17 13:04:10 -0300

    Editado por Paulo Artur depois do horário de aula, devido a uma queda na Internet

--- src/Engenharia/EngenhariaDeSoftware.java
+++ src/Engenharia/EngenhariaDeSoftware.java
@@ -1,6 +1,14 @@
 package Engenharia;
 
+import javax.swing.JOptionPane;
+
 public class EngenhariaDeSoftware {
+	public static void main(String [] args){
+		int a= 3;
+		
+		JOptionPane.showMessageDialog(null, "PROJETO MODIFICADO!! \n por: Paulo Artur");
+		
+	}
   
-	int a= 3;
+	
 }
